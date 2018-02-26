68fbd22efcd8e60f89ad3fd121020223702237a0
Ivaylo Donchev <idonchev@hotmail.com>
2018-02-26 16:57:18 +0200

    sdfg

---
+++ Lecture_3_Classes/Lecture_3_Classes/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_3_Classes
+{
+    public class Circle
+    {
+        public Circle(int r) => radius = r;
+        public Circle() : this(0) {}
+        private int radius;
+        public void SetRadius(int r)
+        {
+            if (r > 0)
+                radius = r;
+            else
+                throw new ArgumentOutOfRangeException("Incorrect radius value");
+        }
+        public double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
--- Lecture_3_Classes/Lecture_3_Classes/Program.cs
+++ Lecture_3_Classes/Lecture_3_Classes/Program.cs
@@ -10,6 +10,26 @@ namespace Lecture_3_Classes
     {
         static void Main(string[] args)
         {
+            Circle c;
+            c = new Circle();
+            c.SetRadius(5);
+            Console.WriteLine($"The area of circle is {c.Area()}");
+
+            Circle d = new Circle(45);
+ 
+            Circle[] circles = new Circle[5];
+            for(int i=0; i<circles.Length; i++)
+            {
+                circles[i] = new Circle();
+                circles[i].SetRadius(i + 1);
+            }
+            foreach(var circle in circles)
+                Console.WriteLine($"Circle with area {circle.Area()}");
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+
         }
     }
 }
