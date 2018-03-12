02f9cea8de9c5af02a80854b9a5a7f2f923d153f
Ivaylo Donchev <idonchev@hotmail.com>
2018-03-12 17:15:19 +0200

    сдфсдфг

---
+++ Lecture_4_Properties/Lecture_4_Properties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_4_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScreenPosition pos = new ScreenPosition(50, 150);
+            Console.WriteLine($"point at possition ({pos.X},{pos.Y})");
+
+            pos.X = 100;
+            pos.Y = 200;
+            Console.WriteLine($"point at possition ({pos.X},{pos.Y})");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lecture_4_Properties/Lecture_4_Properties/ScreenPosition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_4_Properties
+{
+    class ScreenPosition
+    {
+        private int x, y;
+        public ScreenPosition(int x, int y)
+        {
+            this.x = rangeCheckedX(x);
+            this.y = rangeCheckedY(y);
+        }
+        public int X
+        {
+            get => x;
+            set => x = rangeCheckedX(value);
+        }
+        public int Y
+        {
+            get => y;
+            set => y = rangeCheckedY(value);
+        }
+        private static int rangeCheckedX(int x) => (x >= 0 && x <= 1920) ? x : throw new ArgumentOutOfRangeException("X", "argument is out of range");
+        private static int rangeCheckedY(int y) => (y >= 0 && y <= 1080) ? y : throw new ArgumentOutOfRangeException("Y", "argument is out of range");
+    }
+}
