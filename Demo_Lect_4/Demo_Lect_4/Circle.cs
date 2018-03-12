20889c13047b4bbe08e7f74649050de8dca20e9b
Ivaylo Donchev <idonchev@hotmail.com>
2018-03-12 17:26:34 +0200

    sfde

---
+++ Demo_Lect_4/Demo_Lect_4/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Lect_4
+{
+    public class Circle
+    {
+        static int circlesCount;
+        private int radius;
+        static Circle()  // статичен конструктор на инстантен клас
+        {
+            circlesCount = 0;
+        }
+        public Circle() : this(0) { }
+        public Circle(int radius)
+        {
+            this.SetRadius(radius);
+            circlesCount++;
+        }
+        ~Circle()
+        {
+            circlesCount--;
+            Console.WriteLine($"Circle with radius {this.radius} destruction...");
+        }
+        public double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+        public void SetRadius(int radius)
+        {
+            if (radius > 0)
+                this.radius = radius;
+            else
+                throw new ArgumentOutOfRangeException("Incorrect radius value");
+        }
+        public override string ToString() => $"Circle with radius {this.radius}";
+        public static Circle CreateCircle() => new Circle();
+        public static int GetCirclesCount() => circlesCount;
+    }
+}
---
+++ Demo_Lect_4/Demo_Lect_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Lect_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(1);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+
+        }
+    }
+}
