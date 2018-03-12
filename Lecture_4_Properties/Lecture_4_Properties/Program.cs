d1d5c74a1f035349904bff069e7eb21f5c2f23e4
Ivaylo Donchev <idonchev@hotmail.com>
2018-03-12 17:48:22 +0200

    dertge

--- Lecture_4_Properties/Lecture_4_Properties/Program.cs
+++ Lecture_4_Properties/Lecture_4_Properties/Program.cs
@@ -17,6 +17,19 @@ namespace Lecture_4_Properties
             pos.Y = 200;
             Console.WriteLine($"point at possition ({pos.X},{pos.Y})");
 
+            ReadOnlyPoint rop = new ReadOnlyPoint(1, 2);
+            Console.WriteLine($"({rop.X},{rop.Y})");
+            var fields = typeof(ReadOnlyPoint).GetFields(System.Reflection.BindingFlags.NonPublic |
+                System.Reflection.BindingFlags.Instance);
+            foreach(var field in fields)
+                Console.WriteLine(field.Name);
+            fields[0].SetValue(rop, 10);
+            fields[1].SetValue(rop, 20);
+            Console.WriteLine($"({rop.X},{rop.Y})");
+
+            // rop deconstruction
+            var (xx, yy) = rop;
+            Console.WriteLine($"xx = {xx}, yy = {yy}");
             Console.ReadKey();
         }
     }
---
+++ Lecture_4_Properties/Lecture_4_Properties/ReadOnlyPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_4_Properties
+{
+    class ReadOnlyPoint
+    {
+        public int X { get; }
+        public int Y { get; }
+        public ReadOnlyPoint(int x, int y)
+        {
+            X = x; Y = y;
+        }
+        public void Deconstruct(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+}
