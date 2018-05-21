3f18987a2b50ff439577b6f455751d29c1fd795b
Ivaylo Donchev <idonchev@hotmail.com>
2018-05-21 16:38:24 +0300

    wdef

---
+++ Events/Events/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class Counter
+    {
+        public event EventHandler ThresholdReached;
+        protected virtual void OnThresholdReached(EventArgs e)
+        {
+            EventHandler handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        private int count = 0;
+        public void Inc() => count++;
+        public void Dec() => count--;
+        
+    }
+}
---
+++ Events/Events/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void ThresholdReachedEventHandler(object sender, ThresholdReachedEventArgs e);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c = new Counter();
+            for(int i=1; i<=100; i++)
+                c.Inc();
+            c.ThresholdReached += c_ThresholdReached;
+
+            // provide remaining implementation for the class
+            Console.ReadKey();
+        }
+        static void c_ThresholdReached(object sender, EventArgs e)
+        {
+            Console.WriteLine("The threshold was reached.");
+        }
+    }
+}
