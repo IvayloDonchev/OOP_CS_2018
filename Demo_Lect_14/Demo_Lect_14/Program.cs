e07a192405b7f9a6fbe13a3fcfd1473752156584
Ivaylo Donchev <idonchev@hotmail.com>
2018-05-22 09:40:08 +0300

    sdf

---
+++ Demo_Lect_14/Demo_Lect_14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Lect_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine(intStack.Pop());
+                }
+               
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();   
+        }
+    }
+}
